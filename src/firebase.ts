e26ef1f490c75963fbd62b443716c23e84d9bbea
Ananth M <Anantheshwar19@gmail.com>
2026-02-25 18:19:52 +0530

    Fix auth redirect for all devices

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA7bxL4mp0mPaPe0Iy1miN8T7JzVKYFcv8",
-  authDomain: "sree-damodar-transports.onrender.com",
+  authDomain: "sree-damodar-transports.firebaseapp.com",
   projectId: "sree-damodar-transports",
   storageBucket: "sree-damodar-transports.firebasestorage.app",
   messagingSenderId: "918174781719",
@@ -17,16 +17,9 @@ export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-// Use redirect on mobile, popup on desktop
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
 export const signInWithGoogle = async () => {
   await setPersistence(auth, browserLocalPersistence);
-  if (isMobile) {
-    return signInWithRedirect(auth, provider);
-  } else {
-    return signInWithPopup(auth, provider);
-  }
+  return signInWithRedirect(auth, provider);
 };
 
 export const signOutUser = () => signOut(auth);
